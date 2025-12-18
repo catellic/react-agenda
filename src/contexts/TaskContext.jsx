00ef1d562fa433eb9d132904622a3d4c5bf32f5a
catellic <59646804+catellic@users.noreply.github.com>
2025-12-18 12:55:19 +0100

    feat: added localStorage use for tasks

--- src/contexts/TaskContext.jsx
+++ src/contexts/TaskContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid'
 
 const TaskContext = createContext()
@@ -6,12 +6,24 @@ const TaskContext = createContext()
 
 function TaskProvider({ children }) {
 
-    const [tasks, setTasks] = useState([
-        { id: uuidv4(), text: 'Welcome high task!', priority: 'high', status: 'todo' },
-        { id: uuidv4(), text: 'Welcome low task!', priority: 'low', status: 'todo'}])
+    const [tasks, setTasks] = useState(() => {
+        
+        const saved = localStorage.getItem("tasks")
+
+        return (saved 
+            ? JSON.parse(saved)
+            : [
+                { id: uuidv4(), text: 'Welcome high task!', priority: 'high', status: 'todo' },
+                { id: uuidv4(), text: 'Welcome low task!', priority: 'low', status: 'todo' }])
+    })
+
+    useEffect(()=>{
+        localStorage.setItem("tasks", JSON.stringify(tasks))
+    },[tasks])
+
 
     function addTask(taskText, taskPriority, taskStatus) {
-        const newTask = {id: uuidv4(), text: taskText, priority: taskPriority, status: taskStatus}
+        const newTask = { id: uuidv4(), text: taskText, priority: taskPriority, status: taskStatus }
         setTasks([...tasks, newTask])
     }
 
